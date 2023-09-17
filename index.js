b2732ff0d3d454414c560342797676c80c68d814
AadarshKhanna <133052894+AadarshKhanna@users.noreply.github.com>
2023-09-17 17:38:12 +0530

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import U_val from './components/u_val';
-import Tag from './components/Tag';
+import App from './components/App';
 
+const containerStyle = {
+  textAlign: 'center',
+};
 ReactDOM.render(
   <div>
-    <p className='inline-block'>New Post</p>
-    <U_val />
-    <br></br>
-    <br></br>
-    <Tag />
+  <App></App>  
   </div>,
   document.getElementById("root")
 );
