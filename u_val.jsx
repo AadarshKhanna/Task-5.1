9eaa8247e683c42305acabb5610f015c4db266d9
AadarshKhanna <133052894+AadarshKhanna@users.noreply.github.com>
2023-09-17 17:36:19 +0530

    u_val.jsx

--- u_val.jsx
+++ u_val.jsx
@@ -5,9 +5,8 @@ const inputStyle = {
 };
 
 const containerStyle = {
-    display: 'flex', // Use flex display to align items horizontally
-    alignItems: 'center', // Center items vertically within the container
-  };
+  textAlign: 'left',
+};
 
 function U_val() {
   // Define a state variable to store the selected value
@@ -19,48 +18,49 @@ function U_val() {
   return (
     <div>
       <form>
-        <label>Select Post Type:</label>
-        <input
-          type="radio"
-          id="postQuestion"
-          name="post_type"
-          value="Question"
-          onChange={handleRadioChange}
-        />
-        <label for="postQuestion">Question</label>
-        <input
-          type="radio"
-          id="postArticle"
-          name="post_type"
-          value="Article"
-          onChange={handleRadioChange}
-        />
-        <label for="postArticle">Article</label>
+        <div className='alignleft'>
+          <label>Select Post Type:</label>
+          <input
+            type="radio"
+            id="postQuestion"
+            name="post_type"
+            value="Question"
+            onChange={handleRadioChange}
+          />
+          <label htmlFor="postQuestion">Question</label>
+          <input
+            type="radio"
+            id="postArticle"
+            name="post_type"
+            value="Article"
+            onChange={handleRadioChange}
+          />
+          <label htmlFor="postArticle">Article</label>
+        </div>
       </form>
       <p className='inline-block'>What do you want to ask or share</p>
       {selectedValue === 'Question' && (
         <div>
-          <p>This section is designed based on the type of the post.It could be developed by conditional rendering.<span> For post a question, the following section would be appeared.</span></p>
-          <div style={containerStyle}>
-        <label for="title">Title</label>
-        <input
-            id="question"
-            placeholder='Start your question with how, what, why, etc.'
-            style={inputStyle}
-        />
-            </div>
-            <div>
-                <p>Describe you problem</p>
-                <textarea id="msg" name="user_message" style={containerStyle}></textarea>
-            </div>
+          <div>
+            <label htmlFor="title" >Title</label>
+            <input
+              id="question"
+              placeholder='Start your question with how, what, why, etc.'
+              style={inputStyle}
+            />
+          </div>
+          <div>
+            <p className='alignleft'>Describe your problem</p>
+            <br></br>
+            <textarea id="msg" name="user_message"></textarea>
+          </div>
         </div>
       )}
 
       {selectedValue === 'Article' && (
         <div>
-          <p>This section is designed based on the type of the post. It could be developed by conditional rendering.<span> For post an article, the following section would be appeared.</span></p>
-          <div style={containerStyle}>
-            <label for="title">Title</label>
+          <div>
+            <label htmlFor="articleTitle">Title</label>
             <input
               id="articleTitle"
               placeholder="Enter a descriptive title."
@@ -68,13 +68,27 @@ function U_val() {
             />
           </div>
           <div>
-                <p>Abstract</p>
-                <textarea id="msg" name="user_message" style={containerStyle} className='abstract_area' placeholder='Enter a 1-paragraph abstract'></textarea>
-            </div>
+            <p className='alignleft'>Abstract</p>
+            <br></br>
+            <textarea
+              id="msg"
+              name="user_message"
+              style={containerStyle}
+              className='abstract_area'
+              placeholder='Enter a 1-paragraph abstract'
+            ></textarea>
+          </div>
           <div>
-                <p>Article Text</p>
-                <textarea id="msg" name="user_message" style={containerStyle} className='article_area' placeholder='Enter a 1-paragraph abstract'></textarea>
-            </div>
+            <p className='alignleft'>Article Text</p>
+            <br></br>
+            <textarea
+              id="msg"
+              name="user_message"
+              style={containerStyle}
+              className='article_area'
+              placeholder='Enter a 1-paragraph abstract'
+            ></textarea>
+          </div>
         </div>
       )}
     </div>
